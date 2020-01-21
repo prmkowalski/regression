5c8d0cce95d94afdadd0f226baca99f28e0f7f8f
Paweł Kowalski <makr3la@gmail.com>
2020-01-21 08:18:22 +0000

    Add setup metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-import os
+from os import path
 
 from setuptools import setup, find_packages
 import versioneer
 
 from regression import __doc__, __author__
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     install_requires = [line.strip() for line in f.readlines()]
 
 setup(
@@ -19,10 +21,13 @@ setup(
     long_description=long_description,
     url='https://github.com/makr3la/regression',
     author=__author__,
+    author_email='makr3la@gmail.com',
+    license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Flask',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
