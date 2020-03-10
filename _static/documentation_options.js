7bb5c1bceea7d557fb576cf39e543046685db8d8
Travis <travis@travis-ci.org>
2020-03-10 10:46:24 +0000

    Travis build 28 pushed to gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0+5.g8f90409',
+    VERSION: '0.2.0+5.g209ac1f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
