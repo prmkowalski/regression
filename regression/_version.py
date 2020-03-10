f5ba81a6609233b6c28376f3dbe1c0e5943cd6ee
Paweł Kowalski <makr3la@gmail.com>
2020-03-10 23:25:33 +0100

    Change version style to pep440-old

--- regression/_version.py
+++ regression/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-old"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "regression-"
     cfg.versionfile_source = "regression/_version.py"
