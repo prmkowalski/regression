6937ae90870b4eb256c1b360b9f969a5419f3b20
Paweł Kowalski <makr3la@gmail.com>
2020-04-02 18:47:10 +0000

    Fix headers and whitespaces

--- regression/__init__.py
+++ regression/__init__.py
@@ -3,6 +3,7 @@
 __all__ = ['processing']
 __author__ = 'Paweł Kowalski'
 
+from ast import literal_eval
 import os
 
 from flask import Flask, redirect, render_template, request, session
@@ -27,11 +28,12 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     files = processing.find_files()
-    if 'URL' in app.config: files.update(app.config['URL'])
+    if 'URL' in app.config:
+        files.update(app.config['URL'])
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename).replace(' ', '_')
+            filename = secure_filename(file.filename)
             file.save(os.path.join(os.path.dirname(__file__), filename))
             return redirect(request.url)
     return render_template('index.html', version=__version__, files=files)
@@ -40,15 +42,15 @@ def index():
 @app.route('/process', methods=['GET', 'POST'])
 def process():
     if request.method == 'POST':
-        session['name'] = request.form['name']
         session['file'] = request.form['file']
         session.modified = True
-        features = processing.get_xy(session['file'])[0]
+        name, path = literal_eval(session['file'])
+        features = processing.get_xy(path)[0]
         categorical = {f: features[f].unique() for f in features
                        if is_string_dtype(features[f])}
         numerical = [f for f in features if is_numeric_dtype(features[f])]
     return render_template('process.html', version=__version__,
-                           name=session['name'], file=session['file'],
+                           name=name, path=path,
                            categorical=categorical, numerical=numerical)
 
 
@@ -58,34 +60,27 @@ def result():
         session['form'] = dict(request.form.items())
         session.modified = True
         name = session['form'].pop('name', None)
-        file = session['form'].pop('file', None)
-        features, outcome = processing.get_xy(file)
+        path = session['form'].pop('path', None)
+        features, outcome = processing.get_xy(path)
         categorical = {f: features[f].unique() for f in features
                        if is_string_dtype(features[f])}
         category = ('').join(
             [v for k, v in session['form'].items() if k in categorical])
         cf = features[categorical].sum(axis=1)
         if any([not value for value in session['form'].values()]):
-            results = {'<font color="red">Error': 'Failed to collect data'}
+            result = {'<font color="red">Error': 'Failed to collect data'}
         else:
-            X, y, sample = processing.process_data(file, session['form'])
+            X, y, sample = processing.process_data(path, session['form'])
             if category:
                 X = X.loc[(cf[cf == category]).index]
                 y = y.loc[(cf[cf == category]).index]
             ols = processing.predict_ols(X, y, sample, len(X) - 1)
-            ols_url = 'https://en.wikipedia.org/wiki/Ordinary_least_squares'
-            ci_url = 'https://en.wikipedia.org/wiki/Confidence_interval'
-            r2_url = 'https://en.wikipedia.org/wiki/Coefficient_of_determination'
-            results = {
-                f'<strong><a href={ols_url}>OLS</a> Prediction':
-                    f'{ols["mean"]:.5g}',
-                f'</strong>95% <a href={ci_url}>CI</a>':
-                    f'{ols["mean_ci_lower"]:.5g} ÷ {ols["mean_ci_upper"]:.5g}',
-                f'Adjusted <a href={r2_url}>R²</a>':
-                    f'{ols["rsquared_adj"]:.2f}'
+            result = {
+                f'<strong>{ols["mean"]:.5g}</strong>':
+                f'({ols["mean_ci_lower"]:.5g} ÷ {ols["mean_ci_upper"]:.5g})'
             }
             if not all(sample.squeeze(axis=0).between(X.min(), X.max())):
-                results['<font color="orange">Warning'] = 'Out-of-sample'
+                result['<font color="orange">Warning'] = 'Out-of-sample'
     return render_template('result.html', version=__version__,
-                           form=session['form'], name=name, file=file,
-                           outcome=outcome.name, results=results)
+                           form=session['form'], name=name, path=path,
+                           outcome=outcome.name, result=result)
